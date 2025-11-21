254f2c54e684b7924b810a68126e1c3aa481e7dc
Sümeyye Yaşar <sumeyyeyasar54@gmail.com>
2025-11-21 23:46:16 +0300

    Musteri Panel

--- kuafor/kuafor/kuafor/FormMusteriPanel.cs
+++ kuafor/kuafor/kuafor/FormMusteriPanel.cs
@@ -7,14 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using kuafor.Models;
 
 namespace kuafor
 {
     public partial class FormMusteriPanel : Form
     {
-        public FormMusteriPanel()
+        private Musteri _aktifMusteri;
+
+        public FormMusteriPanel(Musteri musteri)
         {
+            _aktifMusteri = musteri;
             InitializeComponent();
+
+            lblHosgeldin.Text = $"Hoş geldin {_aktifMusteri.KullaniciAdi}";
+            LoadRandevular();
+        }
+
+        private void LoadRandevular()
+        {
+            lstRandevular.Items.Clear();
+
+            using (var db = new AppDbContext())
+            {
+                var randevular = db.Randevular
+                    .Where(r => r.MusteriId == _aktifMusteri.Id)
+                    .ToList();
+
+                if (randevular.Count == 0)
+                {
+                    lstRandevular.Items.Add("Randevu bulunmuyor.");
+                    return;
+                }
+
+                foreach (var r in randevular)
+                {
+                    lstRandevular.Items.Add($"{r.Tarih} - {r.HizmetAdi}");
+                }
+            }
+        }
+
+        private void BtnYeniRandevu_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Randevu oluşturma ekranı daha sonra eklenecek.");
+        }
+
+        private void BtnCikis_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
