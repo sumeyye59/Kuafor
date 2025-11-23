4cda9e70db1ff3bc566f4a9cd766b35cfcfe54ec
Sümeyye Yaşar <sumeyyeyasar54@gmail.com>
2025-11-23 23:48:31 +0300

    RandevuForm

---
+++ kuafor/kuafor/kuafor/FormRandevuOlustur.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace kuafor
+{
+    partial class FormRandevuOlustur
+    {
+    
+            private System.ComponentModel.IContainer components = null;
+            private System.Windows.Forms.Label lblBaslik;
+            private System.Windows.Forms.Label lblIslem;
+            private System.Windows.Forms.ComboBox cmbIslem;
+            private System.Windows.Forms.Label lblTarih;
+            private System.Windows.Forms.DateTimePicker dtTarih;
+            private System.Windows.Forms.Button btnKaydet;
+            private System.Windows.Forms.Button btnIptal;
+
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+
+            private void InitializeComponent()
+            {
+                this.lblBaslik = new System.Windows.Forms.Label();
+                this.lblIslem = new System.Windows.Forms.Label();
+                this.cmbIslem = new System.Windows.Forms.ComboBox();
+                this.lblTarih = new System.Windows.Forms.Label();
+                this.dtTarih = new System.Windows.Forms.DateTimePicker();
+                this.btnKaydet = new System.Windows.Forms.Button();
+                this.btnIptal = new System.Windows.Forms.Button();
+                this.SuspendLayout();
+
+                // FORM
+                this.Text = "Yeni Randevu Oluştur";
+                this.ClientSize = new System.Drawing.Size(400, 300);
+                this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+
+                // BAŞLIK
+                lblBaslik.Text = "Yeni Randevu Oluştur";
+                lblBaslik.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+                lblBaslik.AutoSize = true;
+                lblBaslik.Location = new System.Drawing.Point(90, 20);
+
+                // İŞLEM LABEL
+                lblIslem.Text = "Hizmet Seç:";
+                lblIslem.Font = new System.Drawing.Font("Segoe UI", 12F);
+                lblIslem.AutoSize = true;
+                lblIslem.Location = new System.Drawing.Point(40, 90);
+
+                // İŞLEM COMBOBOX
+                cmbIslem.Font = new System.Drawing.Font("Segoe UI", 12F);
+                cmbIslem.Location = new System.Drawing.Point(150, 85);
+                cmbIslem.Size = new System.Drawing.Size(180, 30);
+
+                // TARİH LABEL
+                lblTarih.Text = "Tarih - Saat:";
+                lblTarih.Font = new System.Drawing.Font("Segoe UI", 12F);
+                lblTarih.AutoSize = true;
+                lblTarih.Location = new System.Drawing.Point(40, 140);
+
+                // TARİH PICKER
+                dtTarih.Font = new System.Drawing.Font("Segoe UI", 12F);
+                dtTarih.Location = new System.Drawing.Point(150, 135);
+                dtTarih.Size = new System.Drawing.Size(180, 30);
+                dtTarih.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+                dtTarih.CustomFormat = "dd.MM.yyyy HH:mm";
+
+                // KAYDET
+                btnKaydet.Text = "Kaydet";
+                btnKaydet.Font = new System.Drawing.Font("Segoe UI", 12F);
+                btnKaydet.Location = new System.Drawing.Point(70, 210);
+                btnKaydet.Size = new System.Drawing.Size(120, 40);
+                btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+
+                // İPTAL
+                btnIptal.Text = "İptal";
+                btnIptal.Font = new System.Drawing.Font("Segoe UI", 12F);
+                btnIptal.Location = new System.Drawing.Point(210, 210);
+                btnIptal.Size = new System.Drawing.Size(120, 40);
+                btnIptal.Click += new System.EventHandler(this.btnIptal_Click);
+
+                // FORM KONTROLLERİ
+                this.Controls.Add(lblBaslik);
+                this.Controls.Add(lblIslem);
+                this.Controls.Add(cmbIslem);
+                this.Controls.Add(lblTarih);
+                this.Controls.Add(dtTarih);
+                this.Controls.Add(btnKaydet);
+                this.Controls.Add(btnIptal);
+
+                this.ResumeLayout(false);
+                this.PerformLayout();
+            }
+        
+
+
+
+}
+}
---
+++ kuafor/kuafor/kuafor/FormRandevuOlustur.cs
@@ -0,0 +1,68 @@
+﻿using kuafor.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kuafor
+{
+    public partial class FormRandevuOlustur : Form
+    {
+        private Musteri _musteri;
+        private AppDbContext _db;
+
+        public FormRandevuOlustur(Musteri musteri)
+        {
+            InitializeComponent();
+            _musteri = musteri;
+            _db = new AppDbContext();
+        }
+
+        private void FormRandevuOlustur_Load(object sender, EventArgs e)
+        {
+            cmbIslem.DataSource = _db.Islemler.ToList();
+            cmbIslem.DisplayMember = "Ad";
+            cmbIslem.ValueMember = "Id";
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            int islemId = (int)cmbIslem.SelectedValue;
+            DateTime secilenTarih = dtTarih.Value;
+
+            // Randevu çakışma kontrolü
+            bool cakisma = _db.Randevular
+                .Any(r => r.Baslangic == secilenTarih);
+
+            if (cakisma)
+            {
+                MessageBox.Show("Bu tarihte zaten bir randevu var!", "Hata");
+                return;
+            }
+
+            Randevu r = new Randevu
+            {
+                MusteriId = _musteri.Id,
+                IslemId = islemId,
+                Baslangic = secilenTarih
+            };
+
+            _db.Randevular.Add(r);
+            _db.SaveChanges();
+
+            MessageBox.Show("Randevu başarıyla oluşturuldu!", "Bilgi");
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnIptal_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
